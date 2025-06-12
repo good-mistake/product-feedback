e19d6ff4ddc43812bab2c3fafef6e32bb46cf6da
good-mistake <devillmk88@gmail.com>
2025-06-12 17:46:02 +0330

    as

--- src/lib/generateParams.js
+++ src/lib/generateParams.js
@@ -2,7 +2,18 @@ import { connectDB } from "../utils/db";
 import ProductRequest from "../../models/ProductRequest";
 
 export async function generateParamsFromFeedbacks() {
-  await connectDB();
-  const feedbacks = await ProductRequest.find({}, "_id");
-  return feedbacks.map((f) => ({ id: f._id.toString() }));
+  try {
+    await connectDB();
+    const feedbacks = await ProductRequest.find({}, "_id").lean();
+
+    if (!feedbacks || feedbacks.length === 0) {
+      console.warn("No feedbacks found during build.");
+      return [];
+    }
+
+    return feedbacks.map((f) => ({ id: f._id.toString() }));
+  } catch (error) {
+    console.error("Error in generateParamsFromFeedbacks:", error);
+    return [];
+  }
 }
